72b92201bf3108d4a0c8b305fe6eb4970cec6fdb
urnotzane <urnotzane@163.com>
2018-05-28 18:16:04 +0800

    记住密码

--- src/pages/driver/login/login.js
+++ src/pages/driver/login/login.js
@@ -43,21 +43,40 @@ const task = {
 		})
 	},
 
+	//记住密码按钮状态
+	checkedState: () => {
+		if (!$("#rememberBox").is(":checked")) {
+			setState('account', $("#account").val())
+			setState('password', $("#password").val())
+			setState('rememberDate', Date.now())
+		} else {
+			clearState('account')
+			clearState('password')
+			clearState('rememberDate')
+		}
+	},
+
 	//记住密码
 	rememberPass: () => {
 		mui('body').on('tap', '#remember', function () {
-			if(!$("#rememberBox").is(":checked")){
-				setState('account', $("#account").val())
-				setState('password', $("#password").val())
-			}else{
-				clearState('account')
-				clearState('password')
-			}
+			task.checkedState()
 		})
 	},
 
 	//进入页面判断是否有本地登录信息
+	isLocalLogin: () => {
+		let loginState = ''
+		if (getState('account') && getState('password')) {
+			//如果密码存储时间大于7天 重新登录
+			if ((Date.now() - (getState('rememberDate') * 1)) / 1000 / 60 / 60 / 24 > 7) {
+				loginState = "notLogin"
+			} else {
+				$("#rememberBox").attr("checked", true);
+				loginState = "isLogin"
+			}
+		}
 
+	}
 }
 
 // ios 导航状态
@@ -72,7 +91,11 @@ mui._ready(function () {
 	// 登录时间
 	task.login()
 
+	task.isLocalLogin()
+
 	task.rememberPass()
+
+	task.checkedState()
 
 });
 
